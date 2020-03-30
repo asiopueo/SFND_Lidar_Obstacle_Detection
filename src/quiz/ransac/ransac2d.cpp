cf1171291ba70da7a829b65d17fe5fb18741f12a
Martin <martin.lippl@pueo.de>
2020-03-30 21:45:03 +0200

    Partially implemented Ransac()-function (2 dimensional ransac quiz); maxIterations still to be done

--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -7,6 +7,8 @@
 // using templates for processPointClouds so also include .cpp to help linker
 #include "../../processPointClouds.cpp"
 
+#include <random>
+
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData()
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
@@ -61,25 +63,61 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
+// TODO: Fill in this function
 std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	std::unordered_set<int> inliersResult;
-	srand(time(NULL));
-	
-	// TODO: Fill in this function
+	std::srand ( unsigned ( std::time(0) ) );
+
+	// Just use random_shuffle, as the correct sequence of the points doesn't matter anyway.
+	std::vector<int> randVec;
+	for (uint i=0; i < cloud->points.size(); ++i) randVec.push_back(i);
+
+	uint size = cloud->points.size();
+	pcl::PointXYZ pt1, pt2;
 
 	// For max iterations 
+	for (int i = 0; i<maxIterations; ++i)
+	{
+		// Randomly sample subset and fit line
+		// Shuffle and select the first two points
+		std::random_shuffle(randVec.begin(), randVec.end());
+		pt1 = cloud->points[randVec[0]];
+		pt2 = cloud->points[randVec[1]];
 
-	// Randomly sample subset and fit line
+		// For debugging purposses only:
+		cout << "Point " << randVec[0] << ": " << pt1.x << " , " << pt1.y << endl;
+		cout << "Point " << randVec[1] << ": " << pt2.x << " , " << pt2.y << endl << endl;
 
-	// Measure distance between every point and fitted line
-	// If distance is smaller than threshold count it as inlier
+		float A = pt1.y - pt2.y;
+		float B = pt2.x - pt1.x;
+		float C = pt1.x*pt2.y - pt2.x*pt1.y;
+		float denominator = sqrt(A*A + B*B);
+		float dist = 0;
 
-	// Return indicies of inliers from fitted line with most inliers
+		// Measure distance between every point and fitted line
+		// If distance is smaller than threshold count it as inlier
+		for (int i=0; i<size; i++)
+		{
+			uint index = randVec[i];
+			pcl::PointXYZ pt = cloud->points[index];
+			dist = abs(A*pt.x + B*pt.y + C)/denominator;
+			if (dist < distanceTol)
+				inliersResult.insert(index);
+		}
+
+		// Return indicies of inliers from fitted line with most inliers
+		// TODO
+	}
 	
-	return inliersResult;
+	for (auto inl : inliersResult)
+		cout << inl << ", ";
 
+	cout << endl;
+
+	return inliersResult;
 }
+
 
 int main ()
 {
@@ -92,7 +130,7 @@ int main ()
 	
 
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 0, 0);
+	std::unordered_set<int> inliers = Ransac(cloud, 1, 1.4);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
@@ -124,3 +162,59 @@ int main ()
   	}
   	
 }
+
+
+
+// Maybe useful for gist:
+/*std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	std::unordered_set<int> inliersResult;
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::bernoulli_distribution b;
+	
+	// TODO: Fill in this function
+	std::cout << "Size of set: " << cloud->points.size() << std::endl;
+
+	// For max iterations 
+	for (int i = 0; i<maxIterations; ++i)
+	{
+		//std::cout << b(gen) << std::endl;
+
+		// Randomly sample subset and fit line
+		std::vector<pcl::PointXYZ> subSample;
+		for (auto pt : cloud->points)
+			if (b(gen))	subSample.push_back(pt);
+		
+		// Calculate meanX and meanY of subsample;
+		float meanX = 0, meanY = 0;
+		
+		for (auto pt : subSample)
+		{
+			meanX += pt.x;
+			meanY += pt.y;
+		}
+		//std::cout << meanX << " , " << meanY << std::endl;
+
+		float numerator = 0;
+		float denumerator = 0;
+
+		for (auto i : subSample)
+		{
+			numerator += (i.x - meanX)*(i.y-meanY);
+			denumerator += pow(i.x - meanX, 2);
+		}
+		float m = numerator/denumerator;
+		std::cout << "m = " << m << std::endl; 
+		std::cout << "b = " << meanY-m*meanX << std::endl; 
+
+	}
+
+	// Measure distance between every point and fitted line
+	// If distance is smaller than threshold count it as inlier
+
+	// Return indicies of inliers from fitted line with most inliers
+	
+	return inliersResult;
+
+}*/
